46f055f12a06c03ae4d819c9bc7b84569c1dac87
my-roki <anthonychangrok@gmail.com>
2022-12-16 14:14:46 +0900

    Todo App에 BE 프레임워크 적용

---
+++ src/main/java/com/example/demo/RecommendationController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationController {
+	@GetMapping("/")
+	public String helloWorld() {
+		return "To-do Application !";
+	}
+}
